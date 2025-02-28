6dee40a6214eedd031d9229c891a6f56e02be64c
gabriel7sz <gabriel7sz@outlook.com>
2025-02-28 07:33:12 -0300

    criando o arquivo da aula 002 comentários.php

---
+++ Aula.002-comentarios.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comentários</title>
+</head>
+<body>
+
+    <!-- 
+        No php existe três formas de fazer comentários dentro de um código.
+
+        Pode-se fazer o comentário de uma linha, utilizando o barra barra ( // ).
+        Pode-se fazer o comentário de uma linha no estilo shell, utilizando o sinal sharpe ( # ).
+        Pode-se fazer o comentário de múltiplas linhas, utilizando ( /* */ )
+    -->
+
+    <?php 
+        // Comentário de uma linha
+
+        # Comentário de uma linha
+
+        /*
+            Comentário de múltiplas linhas
+            .
+            .
+        */ 
+    ?>
+    
+</body>
+</html>
