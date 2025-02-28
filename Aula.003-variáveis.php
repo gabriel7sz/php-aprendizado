384d68e0e0f07d43639d02dc54c0434be3c0f963
gabriel7sz <gabriel7sz@outlook.com>
2025-02-28 07:53:41 -0300

    criando arquivo aula003, variáveis.php

---
+++ Aula.003-variáveis.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis</title>
+</head>
+<body>
+
+    <?php 
+        /*
+            Variáveis são utilizadas para o armazenamento temporário de dados.
+
+            No php existem tipos específicos de variáveis.
+            Esses tipos definem o tipo de dado presente na variável. Principais tipos são: 
+            strings: cadeia de caracteres; 
+            int: armazenam valores numéricos inteiros; 
+            float: que também armazena valores numéricos positivos e negativos, mas com fração, ou
+            seja, não inteiros; 
+            boolean: armazenam estados , true ou false; 
+            array: seria um mapa ordenado de variáveis que relacionam-se através de chaves e valores.
+
+            Regras para a declaração de variáveis:
+            Variáveis em php obrigatoriamente necessitam do caractere ( $ ). Esse caractere é quem
+            indica para o interpretador php que naquele ponto do script, está intencionalmente 
+            declarando-se uma variável.
+
+            Variáveis em php não podem conter espaços ou caracteres especiais. A única exceção é
+            o caractere underline ( _ ). 
+
+            O php não requem e nem suporta a definição explícita de tipos. Ou seja, apesar de possuir
+            tipos diferentes de variáveis, não é necessário a indicação do tipo, no momento da declaração.
+            Tipagem dinâmica.
+
+            O php é caseSensitive, ou seja, faz distinção entre letras minúsculas e maiúsculas. Exemplo:
+            $nome != $Nome != $NOME.
+        */
+    ?>
+
+</body>
+</html>
