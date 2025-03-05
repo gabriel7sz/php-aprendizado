76898085ca77671a098c7603fe82b8cad7ce37d4
gabriel7sz <gabriel7sz@outlook.com>
2025-03-05 16:08:00 -0300

    criando arquivo aula 006 if else parte 1]

---
+++ Aula.006-if__else.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Se | Se não</title>
+</head>
+<body>
+    
+    <!-- 
+        O comando if else consiste em uma estrutura de controle. Possiilitando com base de uma respectiva condição,
+        definir diferentes fluxos que a aplicação poderá seguir.
+
+        A sintaxe é em simples, sendo começada com a palavra resevada ( if ) , seguida da CONDIÇÃO, que deve estar
+        contida dentro de parênteses, seguido por abre e fecha chaves ( { } ) .
+
+        Caso a condição seja VERDADEIRA, o bloco de código contido dentro das chaves do comando if é executado.
+        Caso a condição seja FALSA, o bloco dentro da estrutara do if é desconsiderado.
+
+        Já a instrução else, é opcional e se for necessário, é colocado logo após ao bloco de código da instrução if.
+        A instrução else equivale-se ao SE NÃO para a condição estabelecida no if.
+
+        Os comandos if else podem ser encadeados. Ou seja, dentro do primeiro bloco pode-se haver mais condições a 
+        serem testadas, consecutivamente.
+    -->
+
+</body>
+</html>
