621857163eefee2068ba4275b05b5586ffdeee24
gabriel7sz <gabriel7sz@outlook.com>
2025-03-06 15:34:43 -0300

    praticando aula 006 if__else com operadores lógicos e relacionais

--- Aula.006-if__else.php
+++ Aula.006-if__else.php
@@ -26,20 +26,49 @@
 
         <?php 
 
-            // ==
-            // ===
-            // !==
-            // != ou <>
+            //operadors relacionais                   // operadores lógicos
+
+            // ==                                     AND ou &&
+            // ===                                    OR ou ||
+            // !==                                    XOR    
+            // != ou <>                               !
             // <
-            // >
-            // <=
+            // >                        XOR retorna verdadeiro se alguma das expressões for
+            // <=                       verdadeira e a outra falsa.            
             // >=
 
-            if('x' <> 'y') {
+            if(7 >= 4 XOR 10 < 15) {
                 echo 'Verdadeiro';
 
             } else {
                 echo 'Falso';           
+
+            }
+
+            echo '<br />';
+
+            /*
+                Para enteder melhor o operador de negação ( ! ) , usei o exemplo de cima.
+                O que acontece quando usa-se o operador de negação?
+
+                0- É montada a sintaxe noralmente de uma estrutura if, com a adição de um parênteses
+                e o operador de negação. Assim:
+
+                if( !(condição) ) {}
+
+                1- A condição dentro dos parenteses do if é decidida normalmente.
+
+                2- Em seguida com a utilização do parenteses e sinal de negação, basicamente
+                troca-se o resultado para o oposto alcançado na condição. Ou seja, se for true,
+                tornara-se false, e vice-versa. Exemplo:
+            */ 
+
+            //     ( v        v ) = F = !(F) = V
+            if(!(7 >= 4 XOR 10 < 15)) {
+                echo 'Verdadeiro de negação (!)';
+
+            } else {
+                echo 'Falso de negação (!)';
 
             }
 
