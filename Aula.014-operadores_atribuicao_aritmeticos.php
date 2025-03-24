340a1cc8b439f2a90b264c224f2068fa4dc0cb3a
gabriel7sz <gabriel7sz@outlook.com>
2025-03-24 14:34:22 -0300

    criando o arquivo da aula.014

---
+++ Aula.014-operadores_atribuicao_aritmeticos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atribuição de operadoes aritméticos</title>
+</head>
+<body>
+    
+    <?php 
+        $x = 10;
+
+        $x = $x + 5;
+        // ou $x += 5;
+
+        echo $x;
+    ?>
+
+</body>
+</html>
