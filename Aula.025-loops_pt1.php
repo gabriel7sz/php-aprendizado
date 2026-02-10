5b2867ff269f293102dcf8ecffa665d2475f486a
gabriel7sz <gabriel7sz@outlook.com>
2026-02-09 22:35:35 -0300

    explicando a sintaxe do comando while

--- Aula.025-loops_pt1.php
+++ Aula.025-loops_pt1.php
@@ -150,6 +150,105 @@
             </p>
         </div>
 
+        <div class="title" style="margin-top: 20px;">
+            <h1>
+                Como podemos utilizar o laço <b>While</b>.
+            </h1>
+        </div>
+
+        <div class="container__content">
+            <p>
+                Bom, primeiro começaremos com a sintaxe do comando <b>while</b>:
+            </p>
+            
+            <div style="display: flex;">
+                <div style="margin: 20px auto; width: 200px; background: #FFF; border-radius: 8px; padding: 10px;">
+                        <code>
+                            while(condicao) {
+                            }
+                        </code>
+                </div>
+            </div>
+            
+            <p>
+                Faremos alguns testes. Digamos que precisamos de uma lógica em que a instrução do loop, precisa
+                se repetir até uma variável chamada $num1 ser menor que o número 10. Teremos o seguinte código:
+            </p>
+            
+            <div style="margin: 20px auto; width: 500px; background: #FFF; border-radius: 8px; padding: 10px;">
+                <code>
+                    $num1 = 1;<br>
+                    echo "-- Inicio do Loop --";<br>
+                    <br>
+                    while($num1 < 10) {<br>
+                        echo "$num1";<br>
+                        $num1++;<br>
+                    } echo "-- Fim do Loop --"<br>
+
+                </code>
+            </div>
+
+           <p>
+                Segue a execução do código PHP:
+           </p>
+
+           <div style="margin: 20px auto; width: 500px; background: #FFF; border-radius: 8px; padding: 10px;">
+                <?php 
+                    $num1 = 1;
+                    echo "-- Inicio Loop -- <br />";
+
+                    while ($num1 < 10) {
+                        echo "$num1 <br />";
+                        $num1++;
+                    } echo "-- Fim Loop --";
+                ?>
+           </div>
+                
+           <p>
+                Lembrando que a lógica para a parada de execução do loop, não precisa ser simplismente um
+                acréscimo de uma variável. Existe o comando Break, que parará a execução do comando, não 
+                importando aonde o comando break está no escopo do código.
+           </p>
+            <p>
+                Por exemplo:
+            </p>
+            <div style="margin: 20px auto; width: 500px; background: #FFF; border-radius: 8px; padding: 10px;">
+                <code>
+                    $num1 = 1;
+                    <br>
+                    while($num1 < 1000) {<br>
+                        echo "$num1";<br>
+                        $num1 += 100;<br>
+                        if($num1 > 500){<br>
+                            break;<br>
+                        }<br>
+                    } <br>
+                </code>
+            </div>
+            <p>
+                Segue a execução do código PHP:
+            </p>
+
+            <div style="margin: 20px auto; width: 500px; background: #FFF; border-radius: 8px; padding: 10px;">
+                <?php 
+                    $num1 = 0;
+                    echo '-- Inicio Loop --<br />';
+                    while($num1 < 1000) {
+                        echo "$num1 <br />";
+                        $num1 += 100;
+
+                        if ($num1 > 500) {
+                            break;  
+                        }
+                    } echo '-- Fim Loop --<br />';
+                ?>
+            </div>
+
+
+
+        
+        </div>
+
     </div> <!-- Fim container -->
     
 </body>
